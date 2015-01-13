84ffad790fbaec3048d7e0447bbe38a3e29d0d0d
Konrad Rokicki <rokickik@janelia.hhmi.org>
2015-01-13 14:04:28 -0500

    added APP_PACKAGE variable for logging

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=$(dirname $SCRIPT)
 cd "$SCRIPTPATH"
-java -da -Xms512M -Xmx4096M -DAPP_CONFIG=app.properties -DGRID_CONFIG=grid_config.xml -jar derecho.jar
+java -da -Xms512M -Xmx4096M -DAPP_CONFIG=app.properties -DGRID_CONFIG=grid_config.xml -DAPP_PACKAGE=$SCRIPTPATH -jar derecho.jar
